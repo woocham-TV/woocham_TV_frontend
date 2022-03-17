e2b46e561b9205fbd1e9cf31e5c07156da1bc7c7
jiwon <jidole041214@naver.com>
2022-03-17 14:58:53 +0900

    add 앱에 redux 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "./../modules/redux/index";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const store = createStore(rootReducer);
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
-export default MyApp
+export default MyApp;
