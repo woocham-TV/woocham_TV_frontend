7660e7ae59512ebe65da37a09b5a360c7a7eb420
SxxHyun <shp35079@naver.com>
2022-03-20 13:48:46 +0900

    add global css가 적용되도록 import문 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
 import { reduxWrapper } from "../modules/redux/store";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
