2064f0091a517b9c87a4e75c1d0cce48a2df63a6
SxxHyun <shp35079@naver.com>
2022-03-21 11:53:23 +0900

    add 로그인 페이지 퍼블리싱

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,12 +1,75 @@
+import styled from '@emotion/styled';
 import React from 'react';
 import MainLayer from '../../layouts/MainLayer';
+import * as P from '../../styles/common';
 
 const login = () => {
+  const IconArray = [
+    '👨',
+    '👩',
+    '🧑',
+    '👧',
+    '👨‍🦲',
+    '👶',
+    '👵',
+    '👴',
+    '🧔',
+    '👼',
+  ];
+
   return (
     <MainLayer>
-      <div />
+      <MainContainer>
+        <section>
+          <P.PublicTitle>웃참티비 시작하기</P.PublicTitle>
+          <P.PublicInput placeholder="닉네임을 입력하세용~!" />
+        </section>
+        <section>
+          <P.PublicTitle>아이콘 선택</P.PublicTitle>
+          <IconContainer>
+            {IconArray.map((ele, idx) => (
+              <IconSelector key={idx}>{ele}</IconSelector>
+            ))}
+          </IconContainer>
+        </section>
+        <P.PublicButton>시작하기</P.PublicButton>
+      </MainContainer>
     </MainLayer>
   );
 };
+
+const MainContainer = styled(P.PublicContainer)`
+  padding-top: 90px;
+
+  & > section {
+    display: flex;
+    flex-direction: column;
+    gap: 25px;
+  }
+
+  & > section:first-child {
+    margin-bottom: 15px;
+  }
+
+  & > section:nth-child(2) {
+    margin-bottom: 25px;
+  }
+`;
+
+const IconContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  justify-content: space-between;
+  justify-items: center;
+  row-gap: 10px;
+`;
+
+const IconSelector = styled.button`
+  width: 50px;
+  height: 50px;
+  background-color: #f2f2f2;
+  border-radius: 50%;
+  font-size: 24px;
+`;
 
 export default login;
