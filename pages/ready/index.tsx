9c38796bd3bf5a62effa0ca85da8e3277d50aa21
jiwon <jidole041214@naver.com>
2022-04-04 11:10:49 +0900

    update 방송 시작 페이지 퍼블리싱 끝

--- pages/ready/index.tsx
+++ pages/ready/index.tsx
@@ -7,6 +7,9 @@ export default function ReadyToVideo() {
     <MainLayer>
       <Title>방송 시작 준비</Title>
       <Input placeholder="방송 제목을 입력하세용~!" />
+      <ThumbnailSelector>
+        <span>여기를 눌러 썸네일을 선택하세요</span>
+      </ThumbnailSelector>
       <Button>방송 시작</Button>
     </MainLayer>
   );
@@ -24,4 +27,19 @@ const Input = styled(PublicInput)`
 
 const Button = styled(PublicButton)`
   margin-top: 25px;
+`;
+
+const ThumbnailSelector = styled.div`
+  width: 100%;
+  height: 150px;
+  border-radius: 5px;
+  border: 1px dashed #8a8a8a;
+  margin-top: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  & span {
+    color: #848484;
+    font-size: 14px;
+  }
 `;
