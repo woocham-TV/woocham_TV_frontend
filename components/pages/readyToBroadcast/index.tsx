0d7b32ce5af113e6c1c123426c3ede89ded56f97
jiwon <jidole041214@naver.com>
2022-04-20 19:34:46 +0900

    update pages 는 페이지 import 만 하도록

---
+++ components/pages/readyToBroadcast/index.tsx
@@ -0,0 +1,45 @@
+import MainLayer from '../../../layouts/MainLayer';
+import styled from '@emotion/styled';
+import { PublicInput, PublicButton } from '../../../styles/common';
+
+export default function ReadyToBroadcast() {
+  return (
+    <MainLayer>
+      <Title>방송 시작 준비</Title>
+      <Input placeholder="방송 제목을 입력하세용~!" />
+      <ThumbnailSelector>
+        <span>여기를 눌러 썸네일을 선택하세요</span>
+      </ThumbnailSelector>
+      <Button>방송 시작</Button>
+    </MainLayer>
+  );
+}
+
+const Title = styled.h1`
+  font-size: 16px;
+  color: #363636;
+  margin-top: 90px;
+`;
+
+const Input = styled(PublicInput)`
+  margin-top: 20px;
+`;
+
+const Button = styled(PublicButton)`
+  margin-top: 25px;
+`;
+
+const ThumbnailSelector = styled.div`
+  width: 100%;
+  height: 150px;
+  border-radius: 5px;
+  border: 1px dashed #8a8a8a;
+  margin-top: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  & span {
+    color: #848484;
+    font-size: 14px;
+  }
+`;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,5 @@
-import MainLayer from './../layouts/MainLayer';
-import styled from '@emotion/styled';
-import VideoCard from '../components/VideoCard';
+import Main from '../components/pages/main';
 
-export default function Home() {
-  return (
-    <MainLayer>
-      <Title>실시간 웃참방송</Title>
-      <ListWrapper>
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
-      </ListWrapper>
-    </MainLayer>
-  );
+export default function main() {
+  return <Main />;
 }
-
-const Title = styled.h1`
-  color: #363636;
-  font-size: 16px;
-  font-weight: bold;
-  margin-top: 15px;
-`;
-
-const ListWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-top: 15px;
-  gap: 20px;
-`;
