13d17f699c856eef2b7b0c1c96ba85315090b0ef
jiwon <jidole041214@naver.com>
2022-05-11 15:04:22 +0900

    add 방송 정보 입력받기

--- components/LiveReady/index.tsx
+++ components/LiveReady/index.tsx
@@ -1,15 +1,42 @@
 import MainLayer from '../Layout';
 import styled from '@emotion/styled';
 import { PublicInput, PublicButton, PublicTitle } from '../../styles/common';
+import { useState, ChangeEvent } from 'react';
+import { getFileData } from './../../utils/getFileData';
 
 export default function ReadyToBroadcast() {
+  const [title, setTitle] = useState('');
+  const [preview, setPreview] = useState<null | string>(null);
+  const [file, setFile] = useState(null);
+
+  const thumbnailUpload = (event: ChangeEvent<HTMLInputElement>) => {
+    getFileData(event).then(res => {
+      setPreview(res.preview);
+    });
+  };
   return (
     <MainLayer>
       <Title>방송 시작 준비</Title>
-      <Input placeholder="방송 제목을 입력하세용~!" />
-      <ThumbnailSelector>
-        <span>여기를 눌러 썸네일을 선택하세요</span>
-      </ThumbnailSelector>
+      <Input
+        placeholder="방송 제목을 입력하세용~!"
+        onChange={event => {
+          setTitle(event.target.value);
+        }}
+      />
+      <label htmlFor="thumbnail_upload">
+        {preview ? (
+          <ThumbnailImg src={preview} />
+        ) : (
+          <ThumbnailSelector>
+            <input
+              type="file"
+              id="thumbnail_upload"
+              onChange={thumbnailUpload}
+            />
+            <span>여기를 눌러 썸네일을 선택하세요</span>
+          </ThumbnailSelector>
+        )}
+      </label>
       <Button>방송 시작</Button>
     </MainLayer>
   );
@@ -27,17 +54,27 @@ const Button = styled(PublicButton)`
   margin-top: 25px;
 `;
 
+const ThumbnailImg = styled.img`
+  width: 100%;
+  height: 150px;
+  border-radius: 5px;
+  margin-top: 10px;
+`;
+
 const ThumbnailSelector = styled.div`
   width: 100%;
   height: 150px;
   border-radius: 5px;
-  border: 1px dashed #8a8a8a;
   margin-top: 10px;
+  border: 1px dashed #8a8a8a;
   display: flex;
   justify-content: center;
   align-items: center;
   & span {
     color: #848484;
     font-size: 14px;
+  }
+  & input[type='file'] {
+    display: none;
   }
 `;
